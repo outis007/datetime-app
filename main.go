4d496de0874aaf4d93084c02e4ddd0029fa558bc
outis007 <126339424+outis007@users.noreply.github.com>
2025-06-30 20:53:46 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format(time.RFC1123)
+		fmt.Fprintf(w, "Current Date & Time: %s", currentTime)
+	})
+	fmt.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
